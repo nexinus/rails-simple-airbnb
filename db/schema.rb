bd8fe19102e2c754ea3cc29eb740fe513d140e39
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-10-08 22:15:41 +0200

    Add picture url to Flat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_200858) do
+ActiveRecord::Schema.define(version: 2020_10_08_201324) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_10_08_200858) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture_url"
   end
 
 end
